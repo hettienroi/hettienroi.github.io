3a031bd4b45ee2b5b00fbc5c9eec51c8bc8fccaf
Hoang H. Nguyen <hoangnguyen.nchs@gmail.com>
2020-11-23 23:00:24 -0500

    postGenerator v1.0

---
+++ resource/boxes/postGenerator/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace postGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtLinkStore = new System.Windows.Forms.TextBox();
+            this.txtPriceStore = new System.Windows.Forms.TextBox();
+            this.txtLinkBan = new System.Windows.Forms.TextBox();
+            this.txtPriceBan = new System.Windows.Forms.TextBox();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.txtCat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(78, 12);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(66, 20);
+            this.txtCode.TabIndex = 0;
+            // 
+            // txtLinkStore
+            // 
+            this.txtLinkStore.Location = new System.Drawing.Point(92, 23);
+            this.txtLinkStore.Name = "txtLinkStore";
+            this.txtLinkStore.Size = new System.Drawing.Size(100, 20);
+            this.txtLinkStore.TabIndex = 1;
+            // 
+            // txtPriceStore
+            // 
+            this.txtPriceStore.Location = new System.Drawing.Point(92, 49);
+            this.txtPriceStore.Name = "txtPriceStore";
+            this.txtPriceStore.Size = new System.Drawing.Size(100, 20);
+            this.txtPriceStore.TabIndex = 2;
+            // 
+            // txtLinkBan
+            // 
+            this.txtLinkBan.Location = new System.Drawing.Point(265, 23);
+            this.txtLinkBan.Name = "txtLinkBan";
+            this.txtLinkBan.Size = new System.Drawing.Size(100, 20);
+            this.txtLinkBan.TabIndex = 3;
+            // 
+            // txtPriceBan
+            // 
+            this.txtPriceBan.Location = new System.Drawing.Point(265, 49);
+            this.txtPriceBan.Name = "txtPriceBan";
+            this.txtPriceBan.Size = new System.Drawing.Size(100, 20);
+            this.txtPriceBan.TabIndex = 4;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(182, 12);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(101, 20);
+            this.txtSize.TabIndex = 5;
+            // 
+            // txtCat
+            // 
+            this.txtCat.Location = new System.Drawing.Point(345, 12);
+            this.txtCat.Name = "txtCat";
+            this.txtCat.Size = new System.Drawing.Size(55, 20);
+            this.txtCat.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Category:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Product Code:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Size:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Store Link:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Store Price:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(207, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ban Link:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(203, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ban Price:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLinkStore);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtPriceStore);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtLinkBan);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtPriceBan);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(7, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(396, 86);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Price / Link";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(7, 130);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(393, 23);
+            this.btnGen.TabIndex = 15;
+            this.btnGen.Text = "Generate Post";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 161);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCat);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.txtCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Post Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtLinkStore;
+        private System.Windows.Forms.TextBox txtPriceStore;
+        private System.Windows.Forms.TextBox txtLinkBan;
+        private System.Windows.Forms.TextBox txtPriceBan;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.TextBox txtCat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGen;
+    }
+}
+
---
+++ resource/boxes/postGenerator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace postGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            string output = "       <div class=\"column ";
+            output += txtCat.Text.ToString() + "\">\n           <div class=\"content\">\n" +
+                "               <a href=\"";
+            output += txtLinkStore.Text.ToString() + "\" target = \"_blank\" >\n" +
+                "                   <img src=\"pics/";
+            output += txtCode.Text.ToString() + ".jpg\" alt=\"" + txtCode.Text.ToString() + "\" style=\"width: 100 % \">\n" +
+                "               </a>\n" +
+                "               <b>Code: ";
+            output += txtCode.Text.ToString() + "</b>\n" +
+                "               <br>\n" +
+                "               Giá: €";
+            output += txtPriceStore.Text.ToString() + "\n" +
+                "               <br>\n" +
+                "               Bạn: $";
+            output += txtPriceBan.Text.ToString() + "\n" +
+                "               <br>\n" +
+                "               Size: ";
+            output += txtSize.Text.ToString() + " (cm)\n" +
+                "               <br>\n" +
+                "               <center><a href=\"";
+            output += txtLinkBan.Text.ToString() + "\" target=\"_blank\">Link bạn</a></center>\n" +
+                "           </div>\n" +
+                "       </div>";
+            System.Windows.Forms.Clipboard.SetText(output);
+        }
+    }
+}
