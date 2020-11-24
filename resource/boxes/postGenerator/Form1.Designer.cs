e2f7850b81a62d6314cb0042f95708acc5353605
Hoang H. Nguyen <hoangnguyen.nchs@gmail.com>
2020-11-23 23:11:38 -0500

    postGenerator v1.0.1

--- resource/boxes/postGenerator/Form1.Designer.cs
+++ resource/boxes/postGenerator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtCode = new System.Windows.Forms.TextBox();
             this.txtLinkStore = new System.Windows.Forms.TextBox();
             this.txtPriceStore = new System.Windows.Forms.TextBox();
@@ -200,6 +201,7 @@
             this.Controls.Add(this.txtSize);
             this.Controls.Add(this.txtCode);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Post Generator";
